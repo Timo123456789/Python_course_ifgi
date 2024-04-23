7a539b4106f0e3f79407ab03b66edfcb2566cc1f
Ben Giese <ben.giese@uni-muenster.de>
2024-04-23 10:39:47 +0200

    Add Exerecise 3

---
+++ easy_shopping/main.py
@@ -0,0 +1,21 @@
+import calculator
+cal = calculator.calculator()
+
+print(cal.addition(7,5))
+print(cal.substract(34,21))
+print(cal.multiply(54,2))
+print(cal.division(144,2))
+print(cal.division(45,0))
+
+
+import shopping
+shopping = shopping.ShoppingCart()
+shopping.addItem("kiwi", 5)
+shopping.addItem("apple", 7)
+shopping.addItem("blueberriy", 3)
+print(shopping.itemsDict)
+
+shopping.totalAmount()
+
+print(shopping.removeItem("kiwi"))
+shopping.totalAmount()
